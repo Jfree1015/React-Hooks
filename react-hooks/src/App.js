2869958dec970e63ba42b628bbd78b182a84e513
Jfree1015 <july20275@gmail.com>
2023-04-07 00:03:15 +0900

    utilization of useState

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState(1);
+
+  const increaseItem = () => {
+    setItem((currunt) => currunt + 1);
+  };
+
+  const dcreaseItem = () => {
+    setItem((currunt) => currunt - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hello {item}</h1>
+      <h2>my React App</h2>
+      <button onClick={increaseItem}>Increase</button>
+      <button onClick={dcreaseItem}>Dcrease</button>
     </div>
   );
 }
