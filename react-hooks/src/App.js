43be00acb3d0685b0ec86a85cc823afb151802d6
Jfree1015 <july20275@gmail.com>
2023-04-07 00:40:44 +0900

    utilization of useInput

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,22 +1,34 @@
 import { useState } from "react";
 
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  let willUpdate = true;
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
 function App() {
-  const [item, setItem] = useState(1);
-
-  const increaseItem = () => {
-    setItem((currunt) => currunt + 1);
+  const maxLen = (value) => {
+    return value.length <= 10;
   };
-
-  const dcreaseItem = () => {
-    setItem((currunt) => currunt - 1);
-  };
-
+  const name = useInput("Mr.", maxLen);
   return (
     <div>
-      <h1>hello {item}</h1>
+      <h1>hello </h1>
       <h2>my React App</h2>
-      <button onClick={increaseItem}>Increase</button>
-      <button onClick={dcreaseItem}>Dcrease</button>
+      <input placeholder={name.value} {...name} />
     </div>
   );
 }
